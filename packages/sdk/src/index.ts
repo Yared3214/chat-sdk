ed43be52727cb85da15fb7cc23fa1eef1d18db47
Yared3214 <yaredman0099@gmail.com>
2025-09-11 08:12:00 -0700

    updated client.ts

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -1,3 +1,5 @@
-export { ChatClient } from "./client";
-export { initMessaging } from "./messaging";
-export * from "./types";
+// export { ChatClient } from "./client";
+// export { initMessaging } from "./messaging";
+// export * from "./types";
+
+export { SocketClient } from "./client";
